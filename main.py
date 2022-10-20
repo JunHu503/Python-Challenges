0b2562bdd98dd2b4118469acd9c2b2cb98693ae8
Jun Hu Hu <116236987+JunHu503@users.noreply.github.com>
2022-10-20 01:59:43 +0000

    String Handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 Ringgit = "129"
-print(f"This product costs {Ringgit}") 
+print(f"This product costs {Ringgit}")
 
 number = 17
 print(f"Binary: {number:b}\nHexadecimal: {number:x}\nDecimal: {number:d}")
