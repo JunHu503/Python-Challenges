c13fa943a041677274386e7f82dcefdb4176d0a4
JunHu503 <116236987+JunHu503@users.noreply.github.com>
2022-10-27 08:40:15 +0800

    Add files via upload

---
+++ TU10.py
@@ -0,0 +1,9 @@
+import random
+mynumber = random.uniform(1, 9.9)
+print(mynumber)
+
+import random
+Choose_Name = ["James","John","Mark","Rick"]
+for i in range(1,5):
+    chosen = random.choice(Choose_Name)
+    print(chosen)
